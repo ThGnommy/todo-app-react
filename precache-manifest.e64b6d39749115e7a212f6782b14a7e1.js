b5fd6f4fd83904290071d4ba1af5b248a3c67427
ThGnommy <th.brandoli@gmail.com>
2020-07-10 10:17:29 +0200

    Updates

---
+++ precache-manifest.e64b6d39749115e7a212f6782b14a7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef187bcb28f2fe7319ef243bfac65705",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "54d0b188f7f2dc6351a6",
+    "url": "/todo-app-react/static/css/main.6ba4f266.chunk.css"
+  },
+  {
+    "revision": "009f6d970b58666413a2",
+    "url": "/todo-app-react/static/js/2.e73f7fc0.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/todo-app-react/static/js/2.e73f7fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d0b188f7f2dc6351a6",
+    "url": "/todo-app-react/static/js/main.83bce906.chunk.js"
+  },
+  {
+    "revision": "d996bcf66a831bb0953e",
+    "url": "/todo-app-react/static/js/runtime-main.76a8720f.js"
+  }
+]);
